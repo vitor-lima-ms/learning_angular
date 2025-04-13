29a06c63530a65b7f38c241a217acd7b236b50f4
Vitor Lima <viitorlima2000@gmail.com>
2025-04-12 21:47:03 -0300

    Update app.component.ts

--- crud_angular_material/src/app/app.component.ts
+++ crud_angular_material/src/app/app.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { RouterOutlet, RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatIconModule } from '@angular/material/icon'
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
