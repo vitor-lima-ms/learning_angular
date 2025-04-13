0c5a86c6fa4c9ff915c5efc92b6edb0fa630e252
Vitor Lima <viitorlima2000@gmail.com>
2025-04-12 21:47:01 -0300

    Update app.routes.ts

--- crud_angular_material/src/app/app.routes.ts
+++ crud_angular_material/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { ConsultComponent } from './consult/consult.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'register', component: RegisterComponent },
+    { path: 'consult', component: ConsultComponent },
+];
